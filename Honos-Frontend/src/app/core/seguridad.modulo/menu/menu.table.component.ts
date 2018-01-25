e6b3b2b40b0595179d817c9ac19f68e64cd589a1
neckstrauss <neck.strauss@gmail.com>
2018-01-25 13:16:40 -0500

    * se incluyo stilos de validcion para <textate> * se quieto coemntario en menu.table.component

--- Honos-Frontend/src/app/core/seguridad.modulo/menu/menu.table.component.ts
+++ Honos-Frontend/src/app/core/seguridad.modulo/menu/menu.table.component.ts
@@ -10,7 +10,6 @@ import { Component } from "@angular/core";
 export class MenuTableComponent {
 
   constructor(private model: MenuModel) {
-    console.log("paso por table menu");
     model.loadDataSet();
   }
   
